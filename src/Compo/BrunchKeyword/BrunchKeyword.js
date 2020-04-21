f27dce613a8aeed529fd23ae72408a0925287dff
newfull5 <dhtocks@naver.com>
2020-04-21 14:46:21 +0900

    2020.04.21 PM 2:46

--- src/Compo/BrunchKeyword/BrunchKeyword.js
+++ src/Compo/BrunchKeyword/BrunchKeyword.js
@@ -25,9 +25,38 @@ const UnderText = styled.h3`
 
 const TemplateGrid = styled.div`
     display: grid;
-    grid-template-columns: repeat(8, 119px);
-    grid-template-rows: repeat(3, 119px);
-    border: 2px;
+    grid-template-columns: repeat(8, 120px);
+    grid-template-rows: repeat(3, 120px);
+    width: 960px;
+    heigth: 363px;
+    margin: 46px auto 0;
+    position: relative;
+
+`
+
+const Div = styled.div`
+    width: 119px;
+    height: 119px;
+    text-align: center;
+    border: 1px solid #eee;
+    z-index: 1;
+    font-size: 13px;
+    line-height: 18px;
+    &:hover{
+      border-color: #00C3BD;
+      color: #00C3BD;
+      z-index: 2;
+      text-decoration: none;
+      cursor:pointer;
+    }
+`
+
+const Span = styled.span`
+    display: block;
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+    color: #495057;
 `
 
 function Brunchkeyword(props) {
@@ -35,26 +64,41 @@ function Brunchkeyword(props) {
     <div>
       <Keyword />
       <UnderText></UnderText>
-      <div style={{margin:"0 0 0 250px"}}>
-        <TemplateGrid>
-          <div>dhtocks</div>
-          <div>dhtocks</div>
-          <div>dhtocks</div>
-          <div>dhtocks</div>
-          <div>dhtocks</div>
-          <div>dhtocks</div>
-          <div>dhtocks</div>
-          <div>dhtocks</div>
-          <div>dhtocks</div>
-          <div>dhtocks</div>
-          <div>dhtocks</div>
-          <div>dhtocks</div>
-          <div>dhtocks</div>
-          <div>dhtocks</div>
-          <div>dhtocks</div>
-        </TemplateGrid>
+      <div style={{
+
+      }}>
+        <div style={{
+
+        }}>
+          <TemplateGrid>
+            <Div><Span>지구한바퀴<br />세계여행</Span></Div>
+            <Div><Span>그림·웹툰</Span></Div>
+            <Div><Span>시사·이슈</Span></Div>
+            <Div><Span>IT<br />트렌드</Span></Div>
+            <Div><Span>사진·촬영</Span></Div>
+            <Div><Span>취향저격<br />영화 리뷰</Span></Div>
+            <Div><Span>오늘은<br />이런 책</Span></Div>
+            <Div><Span>뮤직 인사이드</Span></Div>
+            <Div><Span>글쓰기<br />코치</Span></Div>
+            <Div><Span>직장인<br />현실 조언</Span></Div>
+            <Div><Span>스타트업<br />경험담</Span></Div>
+            <Div><Span>육아<br />이야기</Span></Div>
+            <Div><Span>요리·레시피</Span></Div>
+            <Div><Span>건강·운동</Span></Div>
+            <Div><Span>멘탈 관리<br />심리 탐구</Span></Div>
+            <Div><Span>디자인<br />스토리</Span></Div>
+            <Div><Span>문화·예술</Span></Div>
+            <Div><Span>건축·설계</Span></Div>
+            <Div><Span>인문학·철학</Span></Div>
+            <Div><Span>쉽게 읽는<br />역사</Span></Div>
+            <Div><Span>우리집<br />반려동물</Span></Div>
+            <Div><Span>멋진<br />캘리그래피</Span></Div>
+            <Div><Span>사랑·이별</Span></Div>
+            <Div><Span>감성<br />에세이</Span></Div>
+          </TemplateGrid>
+        </div>
       </div>
-    </div>
+    </div >
   );
 }
 
